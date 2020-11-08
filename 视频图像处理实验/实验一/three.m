969acd4b4d11323bea8cb995d9d96161b0d3ed41
Pinyk <1748708966@qq.com>
2020-11-08 14:43:09 +0800

    实验一

--- 视频图像处理实验/实验一/three.m
+++ 视频图像处理实验/实验一/three.m
@@ -3,6 +3,10 @@
 a = imread('D:\学习笔记\视频大数据\高宇科-实验一\原图及视频\Lena_256x256.bmp'); 
 b=imnoise(a,'gaussian',0,10);
 c=imnoise(a,'gaussian',0,30);
+d=filter2(fspecial('average',3),b)/255;
+e=filter2(fspecial('average',3),c)/255;
 subplot(321),imshow(a),title('原图像');
-subplot(323),imshow(b),title('加噪声后图像1');
-subplot(324),imshow(c),title('加噪声后图像2');
+subplot(323),imshow(b),title('加噪声后图像(方差10)');
+subplot(324),imshow(c),title('加噪声后图像(方差30)');
+subplot(325),imshow(d),title('3x3邻域平均法(方差10)');
+subplot(326),imshow(e),title('3x3邻域平均法(方差30)');
